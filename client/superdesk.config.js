9c847d66edd2fa39fda821812bd195543be34970
Petr Jasek <jasekpetr@gmail.com>
2019-01-14 15:05:52 +0100

    fix missing superdesk-planning import

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -13,6 +13,7 @@ module.exports = function(grunt) {
         ],
         importApps: [
             'superdesk-analytics',
+            'superdesk-planning',
             '../belga',
         ],
         defaultRoute: '/workspace/personal',
