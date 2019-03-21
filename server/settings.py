b26ad55bda3c2d0833be81907a36254d26d616b3
Karel Petrak <karel.petrak@sourcefabric.org>
2019-03-21 09:26:08 +0100

    added schema and editor config for multimedia (#25)
    * added schema and editor config for multimedia
    
    added schema and editor config for pictures, video, audio
    updated config for VALIDATOR_MEDIA_METADATA
    
    * schema and editor for multimedia under one config

--- server/settings.py
+++ server/settings.py
@@ -89,21 +89,80 @@ ORGANIZATION_NAME_ABBREVIATION = env('ORGANIZATION_NAME_ABBREVIATION', 'Belga')
 
 PUBLISH_QUEUE_EXPIRY_MINUTES = 60 * 24 * 30  # 30d
 
-# media required fields
+# schema for images, video, audio
+SCHEMA = {
+    'picture': {
+        'keywords': {'required': False},
+        'ednote': {'required': False},
+        'headline': {'required': False},
+        'description_text': {'required': True},
+        'byline': {'required': False},
+        'copyrightnotice': {'required': False},
+        'sign_off': {'required': False},
+    },
+    'video': {
+        'keywords': {'required': False},
+        'ednote': {'required': False},
+        'headline': {'required': False},
+        'description_text': {'required': True},
+        'byline': {'required': False},
+        'copyrightnotice': {'required': False},
+        'sign_off': {'required': False},
+    },
+    'audio': {
+        'keywords': {'required': False},
+        'ednote': {'required': False},
+        'headline': {'required': False},
+        'description_text': {'required': True},
+        'byline': {'required': False},
+        'copyrightnotice': {'required': False},
+        'sign_off': {'required': False},
+    },
+}
+
+# editor for images, video, audio
+EDITOR = {
+    'picture': {
+        'keywords': {'order': 1, 'sdWidth': 'full'},
+        'ednote': {'order': 2, 'sdWidth': 'full'},
+        'headline': {'order': 3, 'sdWidth': 'full'},
+        'description_text': {'order': 4, 'sdWidth': 'full'},
+        'byline': {'order': 5, 'sdWidth': 'half'},
+        'copyrightnotice': {'order': 6, 'sdWidth': 'half'},
+        'sign_off': {'order': 7, 'sdWidth': 'half'},
+    },
+    'video': {
+        'keywords': {'order': 1, 'sdWidth': 'full'},
+        'ednote': {'order': 2, 'sdWidth': 'full'},
+        'headline': {'order': 3, 'sdWidth': 'full'},
+        'description_text': {'order': 4, 'sdWidth': 'full'},
+        'byline': {'order': 5, 'sdWidth': 'half'},
+        'copyrightnotice': {'order': 6, 'sdWidth': 'half'},
+        'sign_off': {'order': 7, 'sdWidth': 'half'},
+    },
+    'audio': {
+        'keywords': {'order': 1, 'sdWidth': 'full'},
+        'ednote': {'order': 2, 'sdWidth': 'full'},
+        'headline': {'order': 3, 'sdWidth': 'full'},
+        'description_text': {'order': 4, 'sdWidth': 'full'},
+        'byline': {'order': 5, 'sdWidth': 'half'},
+        'copyrightnotice': {'order': 6, 'sdWidth': 'half'},
+        'sign_off': {'order': 7, 'sdWidth': 'half'},
+    },
+}
+
+# media required fields for upload
 VALIDATOR_MEDIA_METADATA = {
     "headline": {
-        "required": False,
-    },
-    "archive_description": {
         "required": False,
     },
     "description_text": {
         "required": True,
     },
-    "copyrightholder": {
+    "byline": {
         "required": False,
     },
-    "byline": {
+    "copyrightnotice": {
         "required": False,
     },
 }
