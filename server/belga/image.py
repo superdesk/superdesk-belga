e826b8480f7bf2f56436e8f2023447d2007a26a4
Petr Jasek <jasekpetr@gmail.com>
2019-03-22 13:50:02 +0100

    add initial belga coverage support
    using graphic type so we can distinguish between pictures and coverages
    
    SDBELGA-85

--- server/belga/image.py
+++ server/belga/image.py
@@ -5,14 +5,17 @@ import superdesk
 from urllib.parse import urljoin
 from superdesk.utc import local_to_utc
 from superdesk.utils import ListCursor
+from superdesk.text_utils import get_text as _get_text
 
 BELGA_TZ = 'Europe/Brussels'
-GUID_PREFIX = 'urn:belga.be:image:'
 
 
-def get_text(value):
+def get_text(value, strip_html=True):
     try:
-        return value.strip()
+        text = value.strip()
+        if strip_html:
+            text = _get_text(text, lf_on_block=True)
+        return text
     except AttributeError:
         return ''
 
@@ -20,40 +23,6 @@ def get_text(value):
 def get_datetime(value):
     dt = arrow.get(value).datetime
     return local_to_utc(BELGA_TZ, dt)
-
-
-def format_list_item(data):
-    guid = '%s%d' % (GUID_PREFIX, data['imageId'])
-    created = get_datetime(data['createDate'])
-    return {
-        'type': 'picture',
-        'pubstatus': 'usable',
-        '_id': guid,
-        'guid': guid,
-        'headline': get_text(data['name']),
-        'description_text': get_text(data['caption']),
-        'versioncreated': created,
-        'firstcreated': created,
-        'byline': get_text(data.get('author')) or get_text(data['userId']),
-        'creditline': get_text(data['credit']),
-        'source': get_text(data['credit']),
-        'renditions': {
-            'original': {
-                'width': data['width'],
-                'height': data['height'],
-                'href': data['detailUrl'],
-            },
-            'thumbnail': {
-                'href': data['smallUrl'],
-            },
-            'viewImage': {
-                'href': data['previewUrl'],
-            },
-            'baseImage': {
-                'href': data['detailUrl'],
-            },
-        }
-    }
 
 
 class BelgaListCursor(ListCursor):
@@ -68,8 +37,13 @@ class BelgaListCursor(ListCursor):
 
 class BelgaImageSearchProvider(superdesk.SearchProvider):
 
+    GUID_PREFIX = 'urn:belga.be:image:'
+
     label = 'Belga Image'
     base_url = 'https://api.ssl.belga.be/belgaimage-api/'
+    search_endpoint = 'searchImages'
+    items_field = 'images'
+    count_field = 'nrImages'
 
     def __init__(self, provider, **kwargs):
         super().__init__(provider, **kwargs)
@@ -100,24 +74,101 @@ class BelgaImageSearchProvider(superdesk.SearchProvider):
         except KeyError:
             pass
 
-        resp = self.session.get(self.url('searchImages'), params=api_params)
+        resp = self.session.get(self.url(self.search_endpoint), params=api_params)
         if resp.status_code != 200:
             resp.raise_for_status()
 
         data = resp.json()
-        docs = [format_list_item(item) for item in data['images']]
-        return BelgaListCursor(docs, data['nrImages'])
+        docs = [self.format_list_item(item) for item in data[self.items_field]]
+        return BelgaListCursor(docs, data[self.count_field])
 
     def fetch(self, guid):
-        _id = guid.replace(GUID_PREFIX, '')
+        _id = guid.replace(self.GUID_PREFIX, '')
         params = {'i': _id}
         resp = self.session.get(self.url('getImageById'), params=params)
         if resp.status_code != 200:
             resp.raise_for_status()
 
         data = resp.json()
-        return format_list_item(data)
+        return self.format_list_item(data)
+
+    def format_list_item(self, data):
+        guid = '%s%d' % (self.GUID_PREFIX, data['imageId'])
+        created = get_datetime(data['createDate'])
+        return {
+            'type': 'picture',
+            'pubstatus': 'usable',
+            '_id': guid,
+            'guid': guid,
+            'headline': get_text(data['name']),
+            'description_text': get_text(data['caption']),
+            'versioncreated': created,
+            'firstcreated': created,
+            'byline': get_text(data.get('author')) or get_text(data['userId']),
+            'creditline': get_text(data['credit']),
+            'source': get_text(data['credit']),
+            'renditions': {
+                'original': {
+                    'width': data['width'],
+                    'height': data['height'],
+                    'href': data['detailUrl'],
+                },
+                'thumbnail': {
+                    'href': data['smallUrl'],
+                },
+                'viewImage': {
+                    'href': data['previewUrl'],
+                },
+                'baseImage': {
+                    'href': data['detailUrl'],
+                },
+            }
+        }
+
+
+class BelgaCoverageSearchProvider(BelgaImageSearchProvider):
+
+    GUID_PREFIX = 'urn:belga.be:coverage:'
+
+    label = 'Belga Coverage'
+    search_endpoint = 'searchGalleries'
+    items_field = 'galleries'
+    count_field = 'nrGalleries'
+
+    def format_list_item(self, data):
+        guid = '%s%d' % (self.GUID_PREFIX, data['galleryId'])
+        created = get_datetime(data['createDate'])
+        thumbnail = data['iconThumbnailUrl']
+        return {
+            'type': 'graphic',
+            'mimetype': 'application/vnd.belga.coverage',
+            'pubstatus': 'usable',
+            '_id': guid,
+            'guid': guid,
+            'headline': get_text(data['name']),
+            'description_text': get_text(data.get('description')),
+            'versioncreated': created,
+            'firstcreated': created,
+            'byline': get_text(data.get('author')) or get_text(data.get('userId')),
+            'creditline': get_text(data['credit']),
+            'source': get_text(data['credit']),
+            'renditions': {
+                'original': {
+                    'href': thumbnail,
+                },
+                'thumbnail': {
+                    'href': thumbnail,
+                },
+                'viewImage': {
+                    'href': thumbnail,
+                },
+                'baseImage': {
+                    'href': thumbnail,
+                },
+            }
+        }
 
 
 def init_app(app):
     superdesk.register_search_provider('belga_image', provider_class=BelgaImageSearchProvider)
+    superdesk.register_search_provider('belga_coverage', provider_class=BelgaCoverageSearchProvider)
