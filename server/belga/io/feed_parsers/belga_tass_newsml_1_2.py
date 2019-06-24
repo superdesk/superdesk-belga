7ff1603e280ac5d14ce591fb9e1eefca737bbc4f
Thanh Nguyen <thanh.nguyentan@idsolutions.com.vn>
2019-06-24 14:51:10 +0700

    update missing fields for TASS ANP.

--- server/belga/io/feed_parsers/belga_tass_newsml_1_2.py
+++ server/belga/io/feed_parsers/belga_tass_newsml_1_2.py
@@ -29,5 +29,43 @@ class BelgaTASSNewsMLOneFeedParser(BaseBelgaNewsMLOneFeedParser):
         item['firstcreated'] = local_to_utc(tz, item['firstcreated'])
         item['versioncreated'] = local_to_utc(tz, item['firstcreated'])
 
+    def parser_newscomponent(self, item, newscomponent_el):
+        """
+        Example:
+        <NewsComponent Duid="03AE4325838900396A95" Essential="no" EquivalentsList="no">
+            NewsComponent Duid="03AE4325838900396A95">
+                <NewsComponent xml:lang="en"><Role FormalName="Main" />
+                    ....
+                </NewsComponent>
+            </NewsComponent>
+        </NewsComponent>
+        :param item:
+        :param newscomponent_el:
+        :return:
+        """
+        third_newscomponent_el = newscomponent_el.find('NewsComponent/NewsComponent')
+        if third_newscomponent_el is not None:
+            super().parser_newscomponent(item, third_newscomponent_el)
+        if newscomponent_el.attrib.get('Duid') is not None:
+            item['guid'] = newscomponent_el.attrib.get('Duid', '')
+
+            # Essential is CV
+        essential = newscomponent_el.attrib.get('Essential')
+        if essential:
+            item.setdefault('subject', []).append({
+                "name": essential,
+                "qcode": essential,
+                "scheme": "essential"
+            })
+
+        # EquivalentsList is CV
+        equivalents_list = newscomponent_el.attrib.get('EquivalentsList')
+        if equivalents_list:
+            item.setdefault('subject', []).append({
+                "name": equivalents_list,
+                "qcode": equivalents_list,
+                "scheme": "equivalents_list"
+            })
+
 
 register_feed_parser(BelgaTASSNewsMLOneFeedParser.NAME, BelgaTASSNewsMLOneFeedParser())
