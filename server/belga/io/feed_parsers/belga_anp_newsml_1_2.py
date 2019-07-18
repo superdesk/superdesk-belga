4460d5c4dc95e39fd0932db9cb6895c4983148a2
Loi Tran <loi.tran@idsolutions.com.vn>
2019-07-18 10:15:34 +0700

    Update mapping news products from genre for ANP

--- server/belga/io/feed_parsers/belga_anp_newsml_1_2.py
+++ server/belga/io/feed_parsers/belga_anp_newsml_1_2.py
@@ -9,6 +9,7 @@
 # at https://www.appendsourcefabric.org/superdesk/license
 
 from superdesk.io.registry import register_feed_parser
+from superdesk import get_resource_service
 from .base_belga_newsml_1_2 import BaseBelgaNewsMLOneFeedParser
 import pytz
 
@@ -24,6 +25,25 @@ class BelgaANPNewsMLOneFeedParser(BaseBelgaNewsMLOneFeedParser):
         super().parser_newsmanagement(item, manage_el)
         item['firstcreated'] = item['firstcreated'].astimezone(pytz.utc)
         item['versioncreated'] = item['versioncreated'].astimezone(pytz.utc)
+
+    def parse(self, xml, provider=None):
+        items = super().parse(xml, provider)
+        vocabularies = get_resource_service('vocabularies').find_one(req=None, _id='categories').get('items', [])
+        categories = {cat['qcode']: cat['name'] for cat in vocabularies}
+        for item in items:
+            news_products = []
+            for subject in item['subject']:
+                if subject.get('scheme', '') == 'genre':
+                    qcode = subject.get('name')
+                    category = categories.get(qcode, 'GENERAL')
+                    item.setdefault('anpa_category', []).append({'qcode': qcode})
+                    news_products.append({
+                        'name': category,
+                        'qcode': category,
+                        'scheme': 'news_products',
+                    })
+            item['subject'].extend(news_products)
+        return items
 
 
 register_feed_parser(BelgaANPNewsMLOneFeedParser.NAME, BelgaANPNewsMLOneFeedParser())
