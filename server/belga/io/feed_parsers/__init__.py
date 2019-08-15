5c1801439c5411d196b63508fc6b0b3978c29afe
Loi Tran <loi.tran@idsolutions.com.vn>
2019-08-15 16:06:53 +0700

    Add kyodo xml 1.2 parser

--- server/belga/io/feed_parsers/__init__.py
+++ server/belga/io/feed_parsers/__init__.py
@@ -5,5 +5,6 @@ from . import belga_efe_newsml_1_2  # noqa
 from . import belga_tass_newsml_1_2  # noqa
 from . import belga_iptc7901  # noqa
 from . import belga_anpa  # noqa
-from . import belga_dpa_newsml_2_0 # noqa
-from . import belga_ats_newsml_1_2 # noqa
+from . import belga_dpa_newsml_2_0  # noqa
+from . import belga_ats_newsml_1_2  # noqa
+from . import belga_kyodo_newsml_1_2  # noqa
