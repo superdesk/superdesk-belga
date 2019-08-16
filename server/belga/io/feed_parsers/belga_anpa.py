0cbc52c0daf232cbbf7e2aa2223418874228de31
Loi Tran <loi.tran@idsolutions.com.vn>
2019-08-16 11:39:42 +0700

    Add news products mapping for kyodo

--- server/belga/io/feed_parsers/belga_anpa.py
+++ server/belga/io/feed_parsers/belga_anpa.py
@@ -28,6 +28,13 @@ class BelgaANPAFeedParser(ANPAFeedParser):
     NAME = 'belgaanpa1312'
     label = 'Belga anpa1312'
 
+    MAPPING_PRODUCTS = {
+        'F': 'ECONOMY',
+        'P': 'POLITICS',
+        'E': 'CULTURE',
+        'S': 'SPORTS',
+    }
+
     def can_parse(self, file_path):
         try:
             with open(file_path, 'rb') as f:
@@ -54,8 +61,14 @@ class BelgaANPAFeedParser(ANPAFeedParser):
                 b'([0-9]{1,2})-([0-9]{1,2}) ([0-9]{4})',
                 lines[1], flags=re.I)
             if m:
+                qcode = m.group(2).decode().upper()
                 item['priority'] = self.map_priority(m.group(1).decode())
-                item['anpa_category'] = [{'qcode': m.group(2).decode()}]
+                item['anpa_category'] = [{'qcode': qcode}]
+                item['subject'] = [{
+                    'qcode': self.MAPPING_PRODUCTS.get(qcode, 'GENERAL'),
+                    'name': self.MAPPING_PRODUCTS.get(qcode, 'GENERAL'),
+                    'scheme': 'news_products'
+                }]
                 item['slugline'] = m.group(6).decode('latin-1', 'replace')
                 item['anpa_take_key'] = m.group(7).decode('latin-1', 'replace').strip()
                 item['word_count'] = int(m.group(10).decode())
