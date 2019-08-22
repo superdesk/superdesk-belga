8804094f6d348ebb57258fe1ad465542d737410e
Loi Tran <loi.tran@idsolutions.com.vn>
2019-08-22 17:48:55 +0700

    Add mock for TwitterBelga test

--- server/tests/io/feeding_services/twitter_belga_test.py
+++ server/tests/io/feeding_services/twitter_belga_test.py
@@ -1,4 +1,7 @@
 import datetime
+import json
+
+from httmock import HTTMock, urlmatch
 
 from belga.io.feeding_services.twitter_belga import TwitterBelgaFeedingService
 from superdesk.tests import TestCase
@@ -6,9 +9,9 @@ from superdesk.tests import TestCase
 items = [{
     'source': 'twitter',
     'extra': {
-        'tweet_url': 'https://twitter.com/thanhnguyenfs/status/1161474034728460290'
+        'tweet_url': 'https://twitter.com/belga/status/1161474034728460290'
     },
-    'headline': 'thanhnguyenfs: Hello, I am testing link for facebook\nhttps://t.co/KBxMrf1zGk',
+    'headline': 'belga: Hello, I am testing link for facebook\nhttps://t.co/KBxMrf1zGk',
     'type': 'text',
     'guid': 'tag:localhost:5000:2019:73fa4ad84a5fe6aed212563f03cb148d38ab1c6e',
     'versioncreated': datetime.datetime(2019, 8, 14, 3, 6, 3),
@@ -18,23 +21,38 @@ items = [{
 }]
 
 
+@urlmatch(scheme='https', netloc='iframe.ly', path='/api/oembed', query='url=https://t.co/KBxMrf1zGk&api_key=abcdef')
+def iframely_mock(url, request):
+    body = \
+        (
+            '<div class="iframely-embed" style="max-width: 640px;"><div class="i'
+            'framely-responsive" style="padding-bottom: 75.0131%;"><a href="https://www.faceb'
+            'ook.com/TomandJerry/photos/rpp.131583793581459/2759718637434615/?type=3&amp;thea'
+            'ter" data-iframely-url="//cdn.iframe.ly/api/iframe?url=https%3A%2F%2Ft.co%2FKBxM'
+            'rf1zGk&amp;key=7297b5377a04ff05f3378eb7d6e3cf0d"></a></div></div><script async s'
+            'rc="//cdn.iframe.ly/embed.js" charset="utf-8"></script>'
+        )
+    return json.dumps({'html': body})
+
+
 class TwitterBelgaServiceTestCase(TestCase):
 
     def setUp(self):
         provider = {
             "config": {
-                "iframely_key": "9b959025054a1629510c03",
+                "iframely_key": "abcdef",
                 "embed_tweet": True
             }
         }
-        self.items = TwitterBelgaFeedingService().parse_twitter_belga(items, provider)[0]
+        with HTTMock(iframely_mock):
+            self.items = TwitterBelgaFeedingService().parse_twitter_belga(items, provider)[0]
 
     def test_embed_content(self):
         item = self.items[0]
         self.assertEqual(item["source"], "twitter")
-        self.assertEqual(item["extra"], {'tweet_url': 'https://twitter.com/thanhnguyenfs/status/1161474034728460290'})
+        self.assertEqual(item["extra"], {'tweet_url': 'https://twitter.com/belga/status/1161474034728460290'})
         self.assertEqual(item["headline"],
-                         "thanhnguyenfs: Hello, I am testing link for facebook\nhttps://t.co/KBxMrf1zGk")
+                         "belga: Hello, I am testing link for facebook\nhttps://t.co/KBxMrf1zGk")
         self.assertEqual(item["type"], "text")
         self.assertEqual(item["guid"], "tag:localhost:5000:2019:73fa4ad84a5fe6aed212563f03cb148d38ab1c6e")
         self.assertEqual(str(item["versioncreated"]), "2019-08-14 03:06:03")
