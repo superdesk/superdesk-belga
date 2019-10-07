ab7b023c82424f10eee374478e7a9552101afaf0
Thanh Nguyen <thanh.nguyentan@idsolutions.com.vn>
2019-10-07 11:29:47 +0700

    add ingest and test case for belga tip format.

---
+++ server/belga/io/feed_parsers/belga_tip_newsml_1_2.py
@@ -0,0 +1,166 @@
+import logging
+from .belga_newsml_1_2 import BelgaNewsMLOneFeedParser
+from superdesk.io.registry import register_feed_parser
+from superdesk.publish.formatters.newsml_g2_formatter import XML_LANG
+from .base_belga_newsml_1_2 import SkipItemException
+logger = logging.getLogger(__name__)
+
+
+class BelgaTipNewsMLOneFeedParser(BelgaNewsMLOneFeedParser):
+    """Feed Parser which can parse specific Belga News ML xml files."""
+
+    NAME = 'belgatipnewsml12'
+
+    label = 'Belga Tip News ML 1.2 Parser'
+
+    SUPPORTED_ASSET_TYPES = ('TIP')
+
+    def parser_newsitem(self, newsitem_el):
+        """
+        Parse Newsitem element.
+
+        Example:
+
+         <NewsItem xml:lang="fr">
+            <Identification>
+                ....
+            </Identification>
+            <NewsManagement>
+                ....
+            </NewsManagement>
+            <NewsComponent>
+                ....
+            </NewsComponent>
+          </NewsItem>
+
+        :param item:
+        :param newsitem_el:
+        :return:
+        """
+        # Identification
+        self._item_seed.update(
+            self.parser_identification(newsitem_el.find('Identification'))
+        )
+
+        # NewsManagement
+        self._item_seed.update(
+            self.parser_newsmanagement(newsitem_el.find('NewsManagement'))
+        )
+
+        # NewsComponent
+        news_component_1 = newsitem_el.find('NewsComponent')
+        if news_component_1 is not None:
+            # Genre from NewsComponent 1st level
+            for element in news_component_1.findall('DescriptiveMetadata/Genre'):
+                if element.get('FormalName'):
+                    # genre CV
+                    self._item_seed.setdefault('subject', []).append({
+                        "name": element.get('FormalName'),
+                        "qcode": element.get('FormalName'),
+                        "scheme": "genre"
+                    })
+
+            # NewsComponent 2nd level
+            # NOTE: each NewsComponent of 2nd level is a separate item with unique GUID
+            for news_component_2 in news_component_1.findall('NewsComponent'):
+                # create an item
+                item = {**self._item_seed, 'guid': self._item_seed.get("date_id")}
+                
+                # NewsComponent
+                try:
+                    self.parser_newscomponent(item, news_component_2)
+                except SkipItemException:
+                    continue
+
+                # type
+                self.populate_fields(item)
+
+                self._items.append(item)
+
+    def parser_newscomponent(self, item, newscomponent_el):
+        """
+        Parse NewsComponent in NewsItem element.
+
+        Example:
+
+        <NewsComponent>
+          <NewsLines>
+              <DateLine xml:lang="fr">Paris, 9 déc 2018 (AFP) -</DateLine>
+            <HeadLine xml:lang="fr">Un an après, les fans de Johnny lui rendent hommage à Paris</HeadLine>
+            <NewsLine>
+              <NewsLineType FormalName="ProductLine"/>
+              <NewsLineText xml:lang="fr">(Photo+Live Video+Video)</NewsLineText>
+            </NewsLine>
+          </NewsLines>
+          <AdministrativeMetadata>
+            <Provider>
+              <Party FormalName="AFP"/>
+            </Provider>
+          </AdministrativeMetadata>
+          <DescriptiveMetadata>
+            ....
+          </DescriptiveMetadata>
+          <ContentItem>
+            ....
+          </ContentItem>
+        </NewsComponent>
+
+        :param item:
+        :param component_el:
+        :return:
+        """
+        # Role
+        role = newscomponent_el.find('Role')
+        if role is not None:
+            role_name = role.attrib.get('FormalName')
+            if not (role_name and role_name.upper() in self.SUPPORTED_ASSET_TYPES):
+                logger.warning('NewsComponent/Role/FormalName is not supported: "{}". '
+                               'Skiping an "{}" item.'.format(role_name, item['guid']))
+                raise SkipItemException
+        else:
+            logger.warning('NewsComponent/Role was not found. Skiping an "{}" item.'.format(
+                item['guid']
+            ))
+            raise SkipItemException
+
+        # language
+        item['language'] = newscomponent_el.attrib.get(XML_LANG)
+
+        # NewsLines
+        newslines_el = newscomponent_el.find('NewsLines')
+        self.parser_newslines(item, newslines_el)
+
+        # AdministrativeMetadata
+        admin_el = newscomponent_el.find('AdministrativeMetadata')
+        self.parser_administrativemetadata(item, admin_el)
+
+        # DescriptiveMetadata
+        descript_el = newscomponent_el.find('DescriptiveMetadata')
+        self.parser_descriptivemetadata(item, descript_el)
+
+        # get 3rd level NewsComponent
+        # body_html, headline, abstract
+        for formalname, item_key in (('Body', 'body_html'), ('Title', 'headline'), ('Lead', 'abstract')):
+            role = newscomponent_el.find('NewsComponent/Role[@FormalName="{}"]'.format(formalname))
+            if role is not None:
+                newscomponent = role.getparent()
+                datacontent = newscomponent.find('ContentItem/DataContent')
+                format = newscomponent.find('ContentItem/Format')
+
+                if datacontent is not None and format is not None:
+                    if datacontent.text:
+                        item[item_key] = datacontent.text.strip()
+
+                        if item_key == 'body_html':
+                            item[item_key] = self._plain_to_html(item[item_key])
+                else:
+                    logger.warning('Mimetype or DataContent was not found. Skiping an "{}" item.'.format(
+                        item['guid']
+                    ))
+                    raise SkipItemException
+        if not item.get('body_html'):
+            item['body_html'] = item.get('headline')
+        return item
+
+
+register_feed_parser(BelgaTipNewsMLOneFeedParser.NAME, BelgaTipNewsMLOneFeedParser())
---
+++ server/tests/io/feed_parsers/belga_tip_newsml_1_2_test.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013 - 2018 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+
+import os
+import pytz
+import datetime
+from lxml import etree
+from superdesk.tests import TestCase
+from belga.io.feed_parsers.belga_tip_newsml_1_2 import BelgaTipNewsMLOneFeedParser
+
+
+class BelgaNewsMLOneTestCase(TestCase):
+    filename = 'belga_tip_newsml_1_2.xml'
+
+    def setUp(self):
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        fixture = os.path.normpath(os.path.join(dirname, '../fixtures', self.filename))
+        provider = {'name': 'test'}
+        with open(fixture, 'rb') as f:
+            parser = BelgaTipNewsMLOneFeedParser()
+            self.xml_root = etree.parse(f).getroot()
+            self.item = parser.parse(self.xml_root, provider)
+
+    def test_can_parse(self):
+        self.assertTrue(BelgaTipNewsMLOneFeedParser().can_parse(self.xml_root))
+
+    def test_content(self):
+        item = self.item[0]
+        self.assertEqual(item["provider_id"], "belga.be")
+        self.assertEqual(item["date_id"], "20190928T074132")
+        self.assertEqual(item["item_id"], None)
+        self.assertEqual(item["version"], 0)
+        self.assertEqual(item["public_identifier"], "urn:newsml:www.belga.be")
+        self.assertEqual(item["subject"], [{'name': 'NEWS', 'qcode': 'NEWS', 'scheme': 'news_item_types'}, {'name': 'BIN', 'qcode': 'BIN', 'scheme': 'news_services'}, {'name': 'ALG', 'qcode': 'ALG', 'scheme': 'news_products'}])
+        self.assertEqual(str(item["firstcreated"]), "2019-09-28 05:41:32+00:00")
+        self.assertEqual(str(item["versioncreated"]), "2019-09-28 05:41:32+00:00")
+        self.assertEqual(item["pubstatus"], "usable")
+        self.assertEqual(item["guid"], "20190928T074132")
+        self.assertEqual(item["language"], "NL")
+        self.assertEqual(item["byline"], "BELGA")
+        self.assertEqual(item["headline"], "cor 446 - BRAND GEBOUW, hemiksem")
+        self.assertEqual(item["body_html"], "cor 446 - BRAND GEBOUW, hemiksem")
+        self.assertEqual(item["copyrightholder"], "Belga")
+        self.assertEqual(item["line_type"], "1")
+        self.assertEqual(item["keywords"], ['SMS'])
+        self.assertEqual(item["administrative"], {'provider': 'belga.be'})
+        self.assertEqual(item["priority"], 3)
+        self.assertEqual(item["urgency"], 3)
+        self.assertEqual(item["source"], "BELGA")
+        self.assertEqual(item["extra"], {'country': 'BELGIUM'})
+        self.assertEqual(item["type"], "text")
