291e0096c5a1526fb33ed3cf597186155391f97c
Tomas Kikutis <t.kikutis@gmail.com>
2019-10-11 14:59:10 +0200

    use "sign off" as an avatar

--- client/belga/index.tsx
+++ client/belga/index.tsx
@@ -1,6 +1,24 @@
+import React from 'react';
 import angular from 'angular';
-
+import {IUser} from 'superdesk-api'
+import {startApp} from 'superdesk-core/scripts/index';
 import belgaImage from './image';
+
+class UserAvatar extends React.Component<{user: IUser}> {
+    render() {
+        return (
+            <div className="user-avatar" data-test-id="user-avatar">
+                <figure className="avatar avatar--no-margin initials">
+                    {this.props.user.sign_off}
+                </figure>
+            </div>
+        );
+    }
+}
+
+setTimeout(() => {
+    startApp({UserAvatar});
+});
 
 export default angular.module('belga', [
     belgaImage.name,
