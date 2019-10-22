dccd34bf83efe5b1d9c438acf9727236babb2225
Thanh Nguyen <thanh.nguyentan@idsolutions.com.vn>
2019-10-22 14:24:31 +0700

    update use dateid as guid, and add unit-test.

---
+++ server/belga/io/feed_parsers/belga_remote_newsml_1_2.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.appendsourcefabric.org/superdesk/license
+
+from .belga_newsml_1_2 import BelgaNewsMLOneFeedParser, SkipItemException
+
+
+class BelgaRemoteNewsMLOneFeedParser(BelgaNewsMLOneFeedParser):
+    """Feed Parser for Belga specific AFP NewsML."""
+
+    NAME = 'belga_remote_newsml12'
+
+    label = 'Belga Remote News ML 1.2 Parser'
+
+    SUPPORTED_ASSET_TYPES = ('ALERT', 'SHORT', 'TEXT', 'BRIEF', 'ORIGINAL')
+
+    def parser_newsitem(self, newsitem_el):
+        """
+        Parse Newsitem element.
+        Example:
+         <NewsItem xml:lang="fr">
+            <Identification>
+                ....
+            </Identification>
+            <NewsManagement>
+                ....
+            </NewsManagement>
+            <NewsComponent>
+                ....
+            </NewsComponent>
+          </NewsItem>
+        :param newsitem_el:
+        :return:
+        """
+        # Identification
+        self._item_seed.update(
+            self.parser_identification(newsitem_el.find('Identification'))
+        )
+
+        # NewsManagement
+        self._item_seed.update(
+            self.parser_newsmanagement(newsitem_el.find('NewsManagement'))
+        )
+
+        # NewsComponent
+        news_component_1 = newsitem_el.find('NewsComponent')
+        if news_component_1 is not None:
+            # Genre from NewsComponent 1st level
+            for element in news_component_1.findall('DescriptiveMetadata/Genre'):
+                if element.get('FormalName'):
+                    # genre CV
+                    self._item_seed.setdefault('subject', []).append({
+                        "name": element.get('FormalName'),
+                        "qcode": element.get('FormalName'),
+                        "scheme": "genre"
+                    })
+
+            # NewsComponent 2nd level
+            # NOTE: each NewsComponent of 2nd level is a separate item with unique GUID
+            for news_component_2 in news_component_1.findall('NewsComponent'):
+                # create an item
+                item = {**self._item_seed, 'guid': self._item_seed.get("date_id")}
+
+                # NewsComponent
+                try:
+                    self.parser_newscomponent(item, news_component_2)
+                except SkipItemException:
+                    continue
+
+                # type
+                self.populate_fields(item)
+
+                self._items.append(item)
---
+++ server/tests/io/feed_parsers/belga_remote_newsml_1_2_test.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8; -*-
+#
+# This file is part of Superdesk.
+#
+# Copyright 2013 - 2018 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+
+import os
+import pytz
+import datetime
+from lxml import etree
+from superdesk.tests import TestCase
+from belga.io.feed_parsers.belga_remote_newsml_1_2 import BelgaRemoteNewsMLOneFeedParser
+
+
+class BelgaRemoteNewsMLOneTestCase(TestCase):
+    filename = 'belga_remote_newsml_1_2.xml'
+
+    def setUp(self):
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        fixture = os.path.normpath(os.path.join(dirname, '../fixtures', self.filename))
+        provider = {'name': 'test'}
+        parser = BelgaRemoteNewsMLOneFeedParser()
+        with open(fixture, 'rb') as f:
+            self.xml_root = etree.parse(f).getroot()
+            self.item = parser.parse(self.xml_root, provider)
+
+    def test_can_parse(self):
+        self.assertTrue(BelgaRemoteNewsMLOneFeedParser().can_parse(self.xml_root))
+
+    def test_content(self):
+        item = self.item[0]
+        self.assertEqual(item["provider_id"], "belga.be")
+        self.assertEqual(item["date_id"], "20190603T160217")
+        self.assertEqual(item["item_id"], "0")
+        self.assertEqual(item["version"], 0)
+        self.assertEqual(item["public_identifier"], "urn:newsml:www.belga.be")
+        self.assertEqual(item["subject"], [{'name': 'NEWS', 'qcode': 'NEWS', 'scheme': 'news_item_types'},
+                                           {'name': 'BIN', 'qcode': 'BIN', 'scheme': 'news_services'},
+                                           {'name': 'ALG', 'qcode': 'ALG', 'scheme': 'news_products'},
+                                           {'name': 'S1', 'qcode': 'S1', 'scheme': 'label'}])
+        self.assertEqual(str(item["firstcreated"]), "2019-06-03 14:02:17+00:00")
+        self.assertEqual(str(item["versioncreated"]), "2019-06-03 14:02:17+00:00")
+        self.assertEqual(item["pubstatus"], "usable")
+        self.assertEqual(item["guid"], "20190603T160217")
+        self.assertEqual(item["language"], "nl")
+        self.assertEqual(item["byline"], "BELGA")
+        self.assertEqual(item["headline"], "Knooppunt Schijnpoort in Antwerpen hele zomervakantie afgesloten")
+        self.assertEqual(item["copyrightholder"], "Belga")
+        self.assertEqual(item["line_type"], "1")
+        self.assertEqual(item["keywords"], ['BELGIUM', 'MOBILITEIT', 'VERKEER', 'INFRASTRUCTUUR', 'STEDEN', 'BRIEF'])
+        self.assertEqual(item["administrative"], {'foreign_id': '0'})
+        self.assertEqual(item["authors"], [{'name': 'COR 360', 'role': 'CORRESPONDENT'}])
+        self.assertEqual(item["priority"], 3)
+        self.assertEqual(item["urgency"], 3)
+        self.assertEqual(item["source"], "BELGA")
+        self.assertEqual(item["extra"], {'city': 'ANTWERPEN', 'country': 'BELGIUM'})
+        self.assertEqual(item["type"], "text")
+        self.assertEqual(
+            item['body_html'],
+            '<p>Auto&#x27;s, vrachtverkeer en het openbaar vervoer zullen het kruispunt twee '
+            'maanden lang niet kunnen passeren, fietsers en voetgangers wel via een gemengde '
+            'doorgang langs de werf. Lokaal verkeer kan de Noordersingel en Slachthuislaan in'
+            'rijden maar zal een keerbeweging moeten maken aan het kruispunt. Vanop de snelwe'
+            'g zal je via de afrit Deurne wel richting Deurne of Merksem kunnen rijden, maar '
+            'niet naar de binnenstad. De afrit Borgerhout is een alternatief voor het oosteli'
+            'jke deel van het stadscentrum, de afrit Merksem voor het noordelijke. De oprit a'
+            'an het Sportpaleis zal eveneens enkel bereikbaar zijn vanuit Deurne en Merksem.<'
+            '/p><p> De werken kaderen in de grote heraanleg van de Noordersingel, volgens het'
+            ' beeld van de zuidelijke Singel. In elke rijrichting zullen er nog twee rijstrok'
+            'en zijn met een groene berm ertussenin. Aan beide zijden komt een breed fietspad'
+            ' dat afgescheiden is van de rijweg door een groenstrook of vergroende parkeerstr'
+            'ook. Ook de kruispunten worden veiliger gemaakt.</p>'
+        )
