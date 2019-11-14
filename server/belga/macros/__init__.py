8b64aaa0652fd692f95fde93b17ffc431219f403
GyanP <gyanig72@gmail.com>
2019-11-14 11:19:04 +0530

    addressed comment

--- server/belga/macros/__init__.py
+++ server/belga/macros/__init__.py
@@ -5,32 +5,4 @@ This module will get reloaded per request to be up to date.
 Use `superdesk.macro_register.macros.register` for registration.
 """
 
-import os
-import sys
-import imp
-import importlib
-
-
-def load_macros(path):
-    """Load macros from given path
-
-    :param str path:
-    """
-    macros = [f[:-3] for f in os.listdir(path)
-              if f.endswith('.py') and not f.endswith('_test.py') and not f.startswith('__')]
-
-    for macro in macros:
-        try:
-            module = 'belga.macros.{}'.format(macro)
-            if module in sys.modules.keys():
-                m = sys.modules[module]
-                imp.reload(m)
-            else:
-                importlib.import_module(module)
-        except Exception:
-            pass
-
-
-macro_replacement_fields = {'body_html', 'body_text', 'abstract', 'headline', 'slugline', 'description_text'}
-macros_folder = os.path.realpath(os.path.dirname(__file__))
-load_macros(macros_folder)
+import belga.macros.add_prefix_to_headline
