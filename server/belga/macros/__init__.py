62c5158583852b27774c5caa564792039c86777b
GyanP <gyanig72@gmail.com>
2019-11-14 15:39:20 +0530

    remove comments

--- server/belga/macros/__init__.py
+++ server/belga/macros/__init__.py
@@ -1,8 +1,1 @@
-"""Here you can import/implement macros.
-
-This module will get reloaded per request to be up to date.
-
-Use `superdesk.macro_register.macros.register` for registration.
-"""
-
 import belga.macros.add_prefix_to_headline
