db57b6b0e465065f6fa26a1cc211a48ce03f3106
Thanh Nguyen <thanh.nguyentan@idsolutions.com.vn>
2019-11-19 14:44:25 +0700

    Revert "fix flake8"

--- client/belga/index.tsx
+++ client/belga/index.tsx
@@ -1,36 +1,34 @@
-import React from "react";
-import angular from "angular";
-import { IUser } from "superdesk-api";
-import { startApp } from "superdesk-core/scripts/index";
-import belgaImage from "./image";
-import belga360Archive from "./360archive";
-import planningExtension from "superdesk-planning-extension";
-import markForUserExtension from "superdesk-core/scripts/extensions/markForUser/dist/src/extension";
-import belgaCoverageExtension from "../extensions/belgaCoverage/dist/index";
-import videoEditor from "superdesk-core/scripts/extensions/videoEditor/dist/src/extension";
+import React from 'react';
+import angular from 'angular';
+import {IUser} from 'superdesk-api'
+import {startApp} from 'superdesk-core/scripts/index';
+import belgaImage from './image';
+import belga360Archive from './360archive';
+import planningExtension from 'superdesk-planning-extension';
+import markForUserExtension from 'superdesk-core/scripts/extensions/markForUser/dist/src/extension';
+import belgaCoverageExtension from '../extensions/belgaCoverage/dist/index';
 
-class UserAvatar extends React.Component<{ user: IUser }> {
-  render() {
-    return (
-      <div className="user-avatar" data-test-id="user-avatar">
-        <figure className="avatar avatar--no-margin initials">
-          {this.props.user.sign_off}
-        </figure>
-      </div>
-    );
-  }
+class UserAvatar extends React.Component<{user: IUser}> {
+    render() {
+        return (
+            <div className="user-avatar" data-test-id="user-avatar">
+                <figure className="avatar avatar--no-margin initials">
+                    {this.props.user.sign_off}
+                </figure>
+            </div>
+        );
+    }
 }
 
 setTimeout(() => {
-  startApp(
-    [
-      planningExtension,
-      markForUserExtension,
-      belgaCoverageExtension,
-      videoEditor
-    ],
-    { UserAvatar }
-  );
+    startApp([
+        planningExtension,
+        markForUserExtension,
+        belgaCoverageExtension,
+    ],{UserAvatar});
 });
 
-export default angular.module("belga", [belgaImage.name, belga360Archive.name]);
+export default angular.module('belga', [
+    belgaImage.name,
+    belga360Archive.name,
+]);
