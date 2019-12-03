8feca5038e8034ea239a9e54ccd3e695eeca2d8f
GyanP <gyanig72@gmail.com>
2019-12-03 12:45:21 +0530

    change name of the macro

--- server/belga/macros/desk_incoming_rule_macro.py
+++ server/belga/macros/desk_incoming_rule_macro.py
@@ -2,8 +2,8 @@ from superdesk import get_resource_service
 from .set_default_metadata import set_default_metadata
 
 
-def set_default_meta_with_desk_routing(item, **kwargs):
-    """This macro run two macros set_default_metadata and desk_routing."""
+def desk_incoming_rule_macro(item, **kwargs):
+    """This macro runs two macros set_default_metadata and desk_routing."""
 
     macro_service = get_resource_service('macros')
     desk_routing_macro = macro_service.get_macro_by_name('desk_routing')
@@ -12,8 +12,8 @@ def set_default_meta_with_desk_routing(item, **kwargs):
     desk_routing_macro['callback'](new_item, **kwargs)
 
 
-name = 'set_default_meta_with_desk_routing'
-label = 'Set Default Metadata With Desk Routing'
-callback = set_default_meta_with_desk_routing
+name = 'desk_incoming_rule_macro'
+label = 'Desk Incoming Rule Macro'
+callback = desk_incoming_rule_macro
 access_type = 'backend'
 action_type = 'direct'
