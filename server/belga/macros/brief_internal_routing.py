79e0268c73d0757de31cbc6f2a8dd9f18815a061
Petr <petr.jasek@sourcefabric.org>
2020-01-08 16:47:37 +0100

    basic macro implementation

--- server/belga/macros/brief_internal_routing.py
+++ server/belga/macros/brief_internal_routing.py
@@ -1,6 +1,34 @@
+import superdesk
+
+CREDITS = 'credits'
+BRIEF_CREDIT = 'BELGA/AG'
+
+
+def _get_brief_profile():
+    profile = superdesk.get_resource_service('content_types').find_one(req=None, label='Brief')
+    if profile:
+        return profile['_id']
+
+
+def _get_brief_subject(item):
+    subject = item.get('subject') or []
+    try:
+        credit = next((subj for subj in subject if subj.get('scheme') == CREDITS))
+        if 'BELGA' not in credit.get('qcode'):
+            credit['name'] = credit['qcode'] = BRIEF_CREDIT
+    except StopIteration:
+        subject.append({
+            'name': BRIEF_CREDIT,
+            'qcode': BRIEF_CREDIT,
+            'scheme': CREDITS,
+        })
+    return subject
+
 
 def brief_internal_routing(item, **kwargs):
-    pass
+    item['urgency'] = 2
+    item['profile'] = _get_brief_profile()
+    item['subject'] = _get_brief_subject(item)
 
 
 name = 'Brief internal routing'
--- server/tests/macros/brief_internal_routing_macro_test.py
+++ server/tests/macros/brief_internal_routing_macro_test.py
@@ -13,5 +13,17 @@ class BriefInternalRoutingMacroTestCase(TestCase):
         assert macro.access_type == 'backend'
 
     def test_callback(self):
+        profiles = self.app.data.insert('content_types', [
+            {'label': 'Brief'},
+        ])
+
         item = {}
         macro.callback(item)
+
+        self.assertEqual(profiles[0], item['profile'])
+        self.assertEqual(2, item['urgency'])
+        self.assertIn({
+            'name': 'BELGA/AG',
+            'qcode': 'BELGA/AG',
+            'scheme': 'credits',
+        }, item['subject'])
