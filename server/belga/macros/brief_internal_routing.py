7dff929199a561b0e2daf7c4a695ecf190e7cddd
Petr <petr.jasek@sourcefabric.org>
2020-01-09 12:39:56 +0100

    schedule

--- server/belga/macros/brief_internal_routing.py
+++ server/belga/macros/brief_internal_routing.py
@@ -1,4 +1,12 @@
+
 import superdesk
+
+from flask import current_app as app
+from datetime import timedelta
+from superdesk.metadata.item import CONTENT_STATE, PUBLISH_SCHEDULE, SCHEDULE_SETTINGS
+from superdesk.macros.internal_destination_auto_publish import internal_destination_auto_publish
+from apps.archive.common import update_schedule_settings
+
 
 CREDITS = 'credits'
 BRIEF_CREDIT = 'BELGA/AG'
@@ -29,6 +37,16 @@ def brief_internal_routing(item, **kwargs):
     item['urgency'] = 2
     item['profile'] = _get_brief_profile()
     item['subject'] = _get_brief_subject(item)
+    item['status'] = CONTENT_STATE.SCHEDULED
+    item['operation'] = 'publish'
+
+    # schedule +30m
+    item[PUBLISH_SCHEDULE] = item['versioncreated'] + timedelta(minutes=30)
+    item[SCHEDULE_SETTINGS] = {'time_zone': None}
+    update_schedule_settings(item, PUBLISH_SCHEDULE, item[PUBLISH_SCHEDULE])
+
+    # publish
+    internal_destination_auto_publish(item)
 
 
 name = 'Brief internal routing'
--- server/tests/macros/brief_internal_routing_macro_test.py
+++ server/tests/macros/brief_internal_routing_macro_test.py
@@ -1,29 +1,61 @@
 
-from superdesk.tests import TestCase
+import unittest
+import superdesk.tests as tests
+
+from datetime import timedelta
+from superdesk.utc import utcnow
+from superdesk.errors import StopDuplication
+from superdesk.metadata.item import CONTENT_STATE
+from apps.archive.common import SCHEDULE_SETTINGS
 from belga.macros import brief_internal_routing as macro
 
 
-class BriefInternalRoutingMacroTestCase(TestCase):
+class MacroMetadataTestCase(unittest.TestCase):
 
-    def test_metadata(self):
+    def test_macro(self):
         assert hasattr(macro, 'name')
         assert hasattr(macro, 'label')
         assert hasattr(macro, 'callback')
         assert macro.action_type == 'direct'
         assert macro.access_type == 'backend'
 
-    def test_callback(self):
-        profiles = self.app.data.insert('content_types', [
+
+class BriefInternalRoutingMacroTestCase(tests.TestCase):
+
+    def setUp(self):
+        self.profiles = self.app.data.insert('content_types', [
             {'label': 'Brief'},
         ])
 
-        item = {}
-        macro.callback(item)
+    def test_callback(self):
+        now = utcnow()
+        item = {
+            '_id': 'foo',
+            'guid': 'foo',
+            'type': 'text',
+            'state': CONTENT_STATE.PUBLISHED,
+            'task': {},
+            'versioncreated': now,
+        }
 
-        self.assertEqual(profiles[0], item['profile'])
+        with self.assertRaises(StopDuplication):
+            macro.callback(item)
+
+        # test metadata
+        self.assertEqual(self.profiles[0], item['profile'])
         self.assertEqual(2, item['urgency'])
         self.assertIn({
             'name': 'BELGA/AG',
             'qcode': 'BELGA/AG',
             'scheme': 'credits',
         }, item['subject'])
+
+        # test published
+        published = self.app.data.find_one('published', req=None, original_id=item['_id'])
+        self.assertIsNotNone(published)
+        self.assertEqual(CONTENT_STATE.SCHEDULED, published['state'])
+
+        # test schedule
+        schedule = published[SCHEDULE_SETTINGS]['utc_publish_schedule']
+        self.assertGreaterEqual(now + timedelta(minutes=31), schedule)
+        self.assertLessEqual(now + timedelta(minutes=29), schedule)
