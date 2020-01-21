f379ce9236aa32c279df7579952e530f45927ab8
GyanP <gyanig72@gmail.com>
2020-01-21 15:43:55 +0530

    rename macro

--- server/belga/macros/change_headline_and_urgency.py
+++ server/belga/macros/change_headline_and_urgency.py
@@ -1,9 +1,7 @@
-"""This macro adds a prefix string 'TIP' to headline and urgency to 5 of the item"""
+"""This macro adds a prefix string 'TIP' to headline and sets urgency to 5 of the item"""
 
 
-def add_prefix_and_urgency(item, **kwargs):
-    """Add a prefix string TIP to headline"""
-
+def change_headline_and_urgency(item, **kwargs):
     if 'headline' in item:
         item['headline'] = 'TIP ' + item.get('headline', '')
 
@@ -13,8 +11,8 @@ def add_prefix_and_urgency(item, **kwargs):
     return item
 
 
-name = 'Add prefix and urgency to macro'
+name = 'Change Headline and Urgency Macro'
 label = 'Add prefix TIP to headline and set urgency to 5'
-callback = add_prefix_and_urgency
+callback = change_headline_and_urgency
 access_type = 'backend'
 action_type = 'direct'
