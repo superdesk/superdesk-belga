fdd68b4eeca8b214f57a321de46e67522be67af7
GyanP <gyanig72@gmail.com>
2020-01-27 16:26:48 +0530

    Rename TIP to TIPS ingest macro.

--- server/belga/macros/change_headline_and_urgency.py
+++ server/belga/macros/change_headline_and_urgency.py
@@ -1,9 +1,9 @@
-"""This macro adds a prefix string 'TIP' to headline and sets urgency to 5 of the item"""
+"""This macro adds a prefix string 'TIPS ingest macro' to headline and sets urgency to 5 of the item"""
 
 
 def change_headline_and_urgency(item, **kwargs):
     if 'headline' in item:
-        item['headline'] = 'TIP ' + item.get('headline', '')
+        item['headline'] = 'TIPS ingest macro ' + item.get('headline', '')
 
     if 'urgency' in item:
         item['urgency'] = 5
@@ -12,7 +12,7 @@ def change_headline_and_urgency(item, **kwargs):
 
 
 name = 'Change Headline and Urgency Macro'
-label = 'Add prefix TIP to headline and set urgency to 5'
+label = 'Add prefix TIPS ingest macro to headline and set urgency to 5'
 callback = change_headline_and_urgency
 access_type = 'backend'
 action_type = 'direct'
