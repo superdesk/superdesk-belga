ef7a6d544eda0a53f619e0ab47af1aa666bb12a9
Karel Petrak <karel.petrak@sourcefabric.org>
2020-04-27 10:04:49 +0200

    fixed closing } in langOverride

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -65,6 +65,7 @@ module.exports = function(grunt) {
             'en': {
                 'slugline': 'topic',
                 'keywords': 'storytags'
+                }
             },        
             
         search_cvs: [
