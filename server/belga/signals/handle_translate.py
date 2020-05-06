6b2c344bcbd6e3d8955f2fddc2cb7cb7aeb87d10
olegpshenichniy <olegpshenichniy@gmail.com>
2020-05-06 12:58:06 +0200

    Remove videos from a translation + tests.

--- server/belga/signals/handle_translate.py
+++ server/belga/signals/handle_translate.py
@@ -8,11 +8,15 @@ logger = logging.getLogger(__name__)
 
 
 def handle_duplicate(sender, item, original, operation):
+
     def is_belga_archive(item):
         return item.get('type') == CONTENT_TYPE.TEXT and item.get('extra', {}).get('bcoverage')
 
+    def is_video(item):
+        return item.get('type') == CONTENT_TYPE.VIDEO
+
     if operation == 'translate':
-        # remove all belga archive 360 associations
+        # remove all belga-archive-360/video associations
         for k, v in deepcopy(item.get('associations', {})).items():
-            if is_belga_archive(v):
+            if is_belga_archive(v) or is_video(v):
                 del item.get('associations', {})[k]
