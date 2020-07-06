6abea3da7fb04263bb47a8c1422a6c57c8046a0c
Loi Tran <loi.tran@idsolutions.com.vn>
2020-07-06 15:04:28 +0700

    Use loose comparision to avoid Dueby undefined on init item

--- client/extensions/updateComingUpOnChange/src/extension.ts
+++ client/extensions/updateComingUpOnChange/src/extension.ts
@@ -14,7 +14,7 @@ const extension: IExtension = {
                     onUpdate: (current: IArticle, next: IArticle) => {
                         if (
                             // Only trigger save on toggle
-                            (current.extra?.DueBy === null || next.extra?.DueBy === null) &&
+                            (current.extra?.DueBy == null || next.extra?.DueBy == null) &&
                             current.extra?.DueBy !== next.extra?.DueBy
                         ) {
                             superdesk.ui.article.save();
