e882a04607b9a12c2b940d3a1478f939bbdb1b74
Loi Tran <loi.tran@idsolutions.com.vn>
2020-07-16 14:44:38 +0700

    Remove redundant return in onUpdateAfter

--- client/extensions/saveArticleOnComingUpChange/src/extension.ts
+++ client/extensions/saveArticleOnComingUpChange/src/extension.ts
@@ -19,7 +19,6 @@ const extension: IExtension = {
                         ) {
                             superdesk.ui.article.save();
                         }
-                        return Promise.resolve(next);
                     },
                 },
             },
