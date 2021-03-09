21c5eaa6aab70d4d9255e8959ba0ebb4b4681138
GyanP <gyanig72@gmail.com>
2021-03-09 18:59:46 +0530

    Modify 'Set Default Metadata With Translate' macro to keep Storytags (keywords). [SDBELGA-458]

--- server/belga/macros/set_default_metadata_with_translate.py
+++ server/belga/macros/set_default_metadata_with_translate.py
@@ -57,6 +57,10 @@ def set_default_metadata_with_translate(item, **kwargs):
 
     # and now we apply the update
     new_item = archive_service.find_one(req=None, _id=new_id)
+
+    # set keep_keywords to False to keep the keywords of the originally article or not to overwrite the keywords.
+    kwargs['keep_keywords'] = False
+
     set_default_metadata(new_item, **kwargs)
 
     # untoggle coming up
