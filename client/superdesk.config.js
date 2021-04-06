9563183dcdef0c8d45e48eb431c0b8559c0dc3a5
Karel Petrak <karel.petrak@sourcefabric.org>
2021-04-06 09:04:56 +0200

    Hide Embargo [SDBELGA-453] (#297)
    Hide the embargo field settings from the publish panel

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -60,7 +60,11 @@ module.exports = function(grunt) {
             planning: true,
             assignments: true
         },
-
+        
+        ui: {
+            publishEmbargo: false,
+        },
+        
         langOverride: {
             'en': {
                 'slugline': 'topic',
