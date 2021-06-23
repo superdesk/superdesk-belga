109e28f77280f84a71a43eba213e53ae63d48bb9
Petr Jašek <jasekpetr@gmail.com>
2021-06-23 15:32:05 +0200

    fix handle translate macro associations check (#328)
    for case when there was association but it's set to None
    
    SDBELGA-551

--- server/belga/signals/handle_translate.py
+++ server/belga/signals/handle_translate.py
@@ -3,16 +3,17 @@ from copy import deepcopy
 from superdesk.metadata.item import CONTENT_TYPE
 
 
+def is_belga_archive(item):
+    return item.get('type') == CONTENT_TYPE.TEXT and item.get('extra', {}).get('bcoverage')
+
+
+def is_video(item):
+    return item.get('type') == CONTENT_TYPE.VIDEO
+
+
 def handle_duplicate(sender, item, original, operation):
-
-    def is_belga_archive(item):
-        return item.get('type') == CONTENT_TYPE.TEXT and item.get('extra', {}).get('bcoverage')
-
-    def is_video(item):
-        return item.get('type') == CONTENT_TYPE.VIDEO
-
     if operation == 'translate':
         # remove all belga-archive-360/video associations
         for k, v in deepcopy(item.get('associations', {})).items():
-            if is_belga_archive(v) or is_video(v):
+            if v is not None and (is_belga_archive(v) or is_video(v)):
                 del item.get('associations', {})[k]
