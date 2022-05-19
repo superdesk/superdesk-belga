a9c20c733c1345698de65eb45365b7012bd5ccdd
GyanP <gyanig72@gmail.com>
2022-05-19 18:29:21 +0530

    check the Country mapping for AFP ingests [SDBELGA-636] (#391)
    * check the Country mapping for AFP ingests
    
    * Minor changes
    
    * Address comment
    
    * Minor change

--- server/belga/io/feed_parsers/belga_afp_newsml_1_2.py
+++ server/belga/io/feed_parsers/belga_afp_newsml_1_2.py
@@ -20,55 +20,126 @@ logger = logging.getLogger(__name__)
 class BelgaAFPNewsMLOneFeedParser(BaseBelgaNewsMLOneFeedParser):
     """Feed Parser for Belga specific AFP NewsML."""
 
-    NAME = 'belga_afp_newsml12'
-    label = 'Belga specific AFP News ML 1.2 Parser'
+    NAME = "belga_afp_newsml12"
+    label = "Belga specific AFP News ML 1.2 Parser"
     MAPPING_KEYWORDS = {
-        'ECONOMY': ['BOURSE', 'ECONOMIE', 'CONOMIE', 'MARCHES', 'FINANCE', 'BANQUE'],
-        'SPORTS': ['HIPPISME', 'SPORT', 'SPORTS']
+        "ECONOMY": ["BOURSE", "ECONOMIE", "CONOMIE", "MARCHES", "FINANCE", "BANQUE"],
+        "SPORTS": ["HIPPISME", "SPORT", "SPORTS"],
     }
     MAPPING_CATEGORY = {
-        'SPO': 'NEWS/SPORTS',
-        'POL': 'NEWS/POLITICS',
-        'ECO': 'NEWS/ECONOMY'
+        "SPO": "NEWS/SPORTS",
+        "POL": "NEWS/POLITICS",
+        "ECO": "NEWS/ECONOMY",
     }
 
     def parse_newsitem(self, item, newsitem_el):
         super().parse_newsitem(item, newsitem_el)
         # mapping services-products from category, and have only one product
         matching = False
-        for category in item.get('anpa_category', []):
-            qcode = self.MAPPING_CATEGORY.get(category.get('qcode'), 'NEWS/GENERAL')
-            item.setdefault('subject', []).append({
-                'name': qcode,
-                'qcode': qcode,
-                'parent': 'NEWS',
-                'scheme': 'services-products'
-            })
+        for category in item.get("anpa_category", []):
+            qcode = self.MAPPING_CATEGORY.get(category.get("qcode"), "NEWS/GENERAL")
+            item.setdefault("subject", []).append(
+                {
+                    "name": qcode,
+                    "qcode": qcode,
+                    "parent": "NEWS",
+                    "scheme": "services-products",
+                }
+            )
             matching = True
         if not matching:
-            item.setdefault('subject', []).append({
-                'name': 'NEWS/GENERAL',
-                'qcode': 'NEWS/GENERAL',
-                'parent': 'NEWS',
-                'scheme': 'services-products'
-            })
+            item.setdefault("subject", []).append(
+                {
+                    "name": "NEWS/GENERAL",
+                    "qcode": "NEWS/GENERAL",
+                    "parent": "NEWS",
+                    "scheme": "services-products",
+                }
+            )
 
         # add content for headline when it is empty
-        if item.get('urgency') in (1, 2) and not item.get('headline'):
-            for line in get_text(item.get('body_html', ''), lf_on_block=True).split('\n'):
+        if item.get("urgency") in (1, 2) and not item.get("headline"):
+            for line in get_text(item.get("body_html", ""), lf_on_block=True).split(
+                "\n"
+            ):
                 if line.strip():
-                    item['headline'] = 'URGENT: ' + line.strip()
+                    item["headline"] = "URGENT: " + line.strip()
                     break
         # Label must be empty
-        item['subject'] = [i for i in item['subject'] if i.get('scheme') != 'label']
+        item["subject"] = [i for i in item["subject"] if i.get("scheme") != "label"]
         # Source is AFP
-        credit = {"name": 'AFP', "qcode": 'AFP', "scheme": "sources"}
-        item.setdefault('subject', []).append(credit)
+        credit = {"name": "AFP", "qcode": "AFP", "scheme": "sources"}
+        item.setdefault("subject", []).append(credit)
 
-        if item.get('urgency') == 4:
-            item['urgency'] = 3
-
+        if item.get("urgency") == 4:
+            item["urgency"] = 3
         return item
+
+    def parse_descriptivemetadata(self, item, descript_el):
+        """
+        Function parser DescriptiveMetadata in NewsComponent element.
+
+        Example:
+
+            <DescriptiveMetadata>
+                <Location>
+                    <Property FormalName="Country" Value="FRA" />
+                    <Property FormalName="City" Value="Paris" />
+                </Location>
+                <Property FormalName="GeneratorSoftware" Value="libg2" />
+                <Property FormalName="Keyword" Value="France" />
+                <Property FormalName="Keyword" Value="procès" />
+                <Property FormalName="Keyword" Value="assises" />
+                <Property FormalName="Keyword" Value="marches_test" />
+            </DescriptiveMetadata>
+
+        :param item:
+        :param descript_el:
+        :return:
+        """
+        location_el = descript_el.find("Location")
+        if location_el is not None:
+            item["extra"] = {}
+
+            elements = location_el.findall("Property")
+            for element in elements:
+                if element.attrib.get("FormalName", "") == "Country":
+                    country = element.attrib.get("Value")
+                    item["extra"]["country"] = country
+                    # country is cv
+                    item.setdefault("subject", []).extend(self._get_countries(country))
+                if element.attrib.get("FormalName", "") == "City":
+                    item["extra"]["city"] = element.attrib.get("Value")
+
+        elements = descript_el.findall("Property")
+        for element in elements:
+
+            if element.attrib.get("FormalName", "") == "Keyword":
+                data = element.attrib.get("Value")
+                if "keywords" in item:
+                    item["keywords"].append(data)
+                else:
+                    item["keywords"] = [data]
+
+                # store data in original_metadata, countries and belga-keyword CV
+                belga_keyword = self._get_mapped_keywords(
+                    data.upper(), data.upper(), "belga-keywords"
+                )
+                if belga_keyword:
+                    item.setdefault("subject", []).extend(belga_keyword)
+
+                countries = self._get_mapped_keywords(
+                    data.lower(), data.title(), "countries"
+                )
+                if countries:
+                    item.setdefault("subject", []).extend(
+                        self._get_country(countries[0]["qcode"])
+                    )
+
+                if not belga_keyword and not countries:
+                    item.setdefault("subject", []).extend(
+                        [{"name": data, "qcode": data, "scheme": "original-metadata"}]
+                    )
 
 
 register_feed_parser(BelgaAFPNewsMLOneFeedParser.NAME, BelgaAFPNewsMLOneFeedParser())
--- server/tests/io/feed_parsers/belga_afp_newsml_1_2_test.py
+++ server/tests/io/feed_parsers/belga_afp_newsml_1_2_test.py
@@ -17,14 +17,14 @@ from tests import TestCase
 
 
 class BelgaAFPNewsMLOneTestCase(TestCase):
-    filename = 'afp_belga.xml'
+    filename = "afp_belga.xml"
 
     def setUp(self):
         super().setUp()
         dirname = os.path.dirname(os.path.realpath(__file__))
-        fixture = os.path.normpath(os.path.join(dirname, '../fixtures', self.filename))
-        provider = {'name': 'test'}
-        with open(fixture, 'rb') as f:
+        fixture = os.path.normpath(os.path.join(dirname, "../fixtures", self.filename))
+        provider = {"name": "test"}
+        with open(fixture, "rb") as f:
             parser = BelgaAFPNewsMLOneFeedParser()
             self.xml_root = etree.parse(f).getroot()
             self.item = parser.parse(self.xml_root, provider)
@@ -35,100 +35,111 @@ class BelgaAFPNewsMLOneTestCase(TestCase):
     def test_content(self):
         item = self.item[0]
         self.assertEqual(item["ingest_provider_sequence"], "0579")
-        item["subject"].sort(key=lambda i: i['name'])
+        item["subject"].sort(key=lambda i: i["name"])
         expected_subjects = [
-            {'name': 'AFP', 'qcode': 'AFP', 'scheme': 'sources'},
-            {'name': 'AMN-TFG-1=AMW', 'qcode': 'AMN-TFG-1=AMW', 'scheme': 'of_interest_to'},
-            {'name': 'ARC-TFG-1=ELU', 'qcode': 'ARC-TFG-1=ELU', 'scheme': 'of_interest_to'},
-            {'name': 'DAB-TFG-1=DAB', 'qcode': 'DAB-TFG-1=DAB', 'scheme': 'of_interest_to'},
-            {'name': 'EUA-TFG-1=EUA', 'qcode': 'EUA-TFG-1=EUA', 'scheme': 'of_interest_to'},
-            {'name': 'France', 'qcode': 'country_fra', 'translations': {
-                'name': {'nl': 'FRANKRIJK', 'fr': 'FRANCE'}}, 'scheme': 'country'},
-            {'name': 'France', 'qcode': 'fra', 'translations': {
-                'name': {'nl': 'Frankrijk', 'fr': 'France'}}, 'scheme': 'countries'},
-            {'name': 'MOA-TFG-1=MOA', 'qcode': 'MOA-TFG-1=MOA', 'scheme': 'of_interest_to'},
-            {'name': 'NEWS/ECONOMY', 'qcode': 'NEWS/ECONOMY', 'parent': 'NEWS', 'scheme': 'services-products'},
-            {'name': 'NEWS/GENERAL', 'qcode': 'NEWS/GENERAL', 'parent': 'NEWS', 'scheme': 'services-products'},
-            {'name': 'NEWS/SPORTS', 'qcode': 'NEWS/SPORTS', 'parent': 'NEWS', 'scheme': 'services-products'},
-            {'name': 'POLICE', 'qcode': 'POLICE', 'translations': {
-                'name': {'nl': 'POLITIE', 'fr': 'POLICE'}}, 'scheme': 'belga-keywords'},
-            {'name': 'assises', 'qcode': 'assises', 'scheme': 'original-metadata'},
-            {'qcode': '02000000', 'name': 'crime, law and justice', 'scheme': 'iptc_subject_codes'},
-            {'name': 'default', 'qcode': 'default', 'scheme': 'distribution'},
-            {'name': 'drogues', 'qcode': 'drogues', 'scheme': 'original-metadata'},
-            {'qcode': '02001004', 'name': 'drug trafficking', 'scheme': 'iptc_subject_codes'},
-            {'name': 'marches_test', 'qcode': 'marches_test', 'scheme': 'original-metadata'},
-            {'qcode': '02003000', 'name': 'police', 'scheme': 'iptc_subject_codes'},
-            {'name': 'procès', 'qcode': 'procès', 'scheme': 'original-metadata'},
-            {'qcode': '02008000', 'name': 'trials', 'scheme': 'iptc_subject_codes'}
+            {"name": "AFP", "qcode": "AFP", "scheme": "sources"},
+            {
+                "name": "Bangladesh",
+                "qcode": "country_bgd",
+                "translations": {"name": {"nl": "BANGLADESH", "fr": "BANGLADESH"}},
+                "scheme": "country",
+            },
+            {
+                "name": "France",
+                "qcode": "fra",
+                "translations": {"name": {"nl": "Frankrijk", "fr": "France"}},
+                "scheme": "countries",
+            },
+            {
+                "name": "NEWS/GENERAL",
+                "qcode": "NEWS/GENERAL",
+                "parent": "NEWS",
+                "scheme": "services-products",
+            },
+            {
+                "name": "POLICE",
+                "qcode": "POLICE",
+                "translations": {"name": {"nl": "POLITIE", "fr": "POLICE"}},
+                "scheme": "belga-keywords",
+            },
+            {"name": "assises", "qcode": "assises", "scheme": "original-metadata"},
+            {"name": "default", "qcode": "default", "scheme": "distribution"},
+            {"name": "drogues", "qcode": "drogues", "scheme": "original-metadata"},
+            {
+                "name": "marches_test",
+                "qcode": "marches_test",
+                "scheme": "original-metadata",
+            },
+            {"name": "procès", "qcode": "procès", "scheme": "original-metadata"},
         ]
-        expected_subjects.sort(key=lambda i: i['name'])
+        expected_subjects.sort(key=lambda i: i["name"])
         self.assertEqual(item["subject"], expected_subjects)
         self.assertEqual(item["priority"], 4)
         self.assertEqual(item["provider_id"], "afp.com")
         self.assertEqual(item["date_id"], "20190121T104233Z")
         self.assertEqual(item["item_id"], "TX-PAR-RHO61")
         self.assertEqual(item["version"], "1")
-        self.assertEqual(item["guid"], "urn:newsml:afp.com:20190121T104233Z:TX-PAR-RHO61:1")
+        self.assertEqual(
+            item["guid"], "urn:newsml:afp.com:20190121T104233Z:TX-PAR-RHO61:1"
+        )
         self.assertEqual(str(item["firstcreated"]), "2019-01-21 10:42:33+00:00")
         self.assertEqual(str(item["versioncreated"]), "2019-01-21 10:42:34+00:00")
         self.assertEqual(item["pubstatus"], "usable")
         self.assertEqual(item["urgency"], 3)
-        self.assertEqual(item["associated_with"], {'item': None, 'type': ['Sketch', 'Photo', 'Video']})
-        self.assertEqual(item["dateline"], {'text': 'Paris, 21 jan 2019 (AFP) -',
-                                            'date': datetime.datetime(2019, 1, 21, 10, 42, 33,
-                                                                      tzinfo=datetime.timezone.utc)})
-        self.assertEqual(item["headline"],
-                         "France: deux ex-policiers aux frontières devant la justice pour trafic de cocaïne")
+        self.assertEqual(
+            item["associated_with"],
+            {"item": None, "type": ["Sketch", "Photo", "Video"]},
+        )
+        self.assertEqual(
+            item["headline"],
+            "France: deux ex-policiers aux frontières devant la justice pour trafic de cocaïne",
+        )
         self.assertEqual(item["line_type"], "ProductLine")
         self.assertEqual(item["line_text"], "(Croquis d'audience+Photo+Video)")
-        self.assertEqual(item["administrative"], {'provider': 'AFP'})
-        self.assertEqual(item["language"], "fr")
-        self.assertEqual(item["anpa_category"], [{'qcode': 'SPO'}, {'qcode': 'CLJ'}, {'qcode': 'ECO'}])
-        self.assertEqual(item["extra"], {'how_present': 'Origin', 'country': 'FRA', 'city': 'Paris'})
-        self.assertEqual(item["generator_software"], "libg2")
+        self.assertEqual(item["administrative"], {"provider": "AFP"})
+        self.assertEqual(item["extra"], {"country": "FRA", "city": "Paris"})
         self.assertEqual(item["keywords"], [])
         self.assertEqual(item["slugline"], None)
         self.assertEqual(item["type"], "text")
         self.assertEqual(item["format"], "NITF3.1")
-        self.assertEqual(item["characteristics"], {'size_bytes': '1620', 'word_count': '269'})
-        expected_body = \
-            (
-                "\n<p><br/></p><p>   </p>"
-                "\n<p> Le procès de deux anciens fonctionnaires de la police aux frontières (PAF) de l'aéroport parisie"
-                "n Roissy-Charles de Gaulle, accusés d'avoir facilité l'importation de cocaïne de retour de Républiqu"
-                "e dominicaine, s'est ouvert lundi devant un tribunal à Paris. </p>\n<p>Clément Geisse, 42 ans, et Chr"
-                'istophe Peignelin, 56 ans, comparaissent notamment pour importation de stupéfiants en bande organisé'
-                "e - un crime passible de trente ans de réclusion - et corruption passive, devant la cour d'assises s"
-                'péciale, composée de magistrats professionnels, sans jury. </p>\n<p>Les deux hommes aux cheveux bruns'
-                " coupés courts, l'un la mâchoire carrée, l'autre le visage émacié, ont pénétré dans le box des accus"
-                "és, déclinant d'un ton grave leur identité et leur ancienne profession.</p>\n<p>Ils ont reconnu avoir"
-                ' permis sept à neuf passages de "mules" aux valises chargées de cocaïne en les aidant à contourner l'
-                "es contrôles de l'aéroport de Roissy, entre 2010 et le 25 janvier 2015, date de leur interpellation."
-                ' Ils venaient de tenter de faire sortir deux valises contenant chacune 20 kilos de drogue. </p>\n<p>A'
-                " l'autre extrémité du box, figure Kamel Berkaoui, 43 ans, patron présumé du réseau démantelé à la su"
-                'ite d\'une enquête dénommée "Excédent bagage", menée par l\'Office central pour la répression du trafi'
-                'c illicite des stupéfiants (Ocrtis). </p>\n<p>Neuf membres présumés de son organisation sont égalemen'
-                "t jugés par la cour. </p>\n<p>Les enquêteurs n'ont pas réussi à déterminer quelle quantité de drogue "
-                'avait pu être importée depuis Punta Cana en République dominicaine. </p>\n<p>Les ex-policiers auraien'
-                't empoché au total entre 540.000 et 620.000 euros, à raison de 40.000 euros par "mule" récupérée, se'
-                'lon une estimation fournie en interrogatoire par Clément Geisse. </p>\n<p>Le verdict est attendu le 7'
-                ' février. </p>\n<p>asl/tib/pid/cac</p>\n\n'
-
-            )
+        self.assertEqual(
+            item["characteristics"], {"size_bytes": "1620", "word_count": "269"}
+        )
+        expected_body = (
+            "\n<p><br/></p><p>   </p>"
+            "\n<p> Le procès de deux anciens fonctionnaires de la police aux frontières (PAF) de l'aéroport parisie"
+            "n Roissy-Charles de Gaulle, accusés d'avoir facilité l'importation de cocaïne de retour de Républiqu"
+            "e dominicaine, s'est ouvert lundi devant un tribunal à Paris. </p>\n<p>Clément Geisse, 42 ans, et Chr"
+            "istophe Peignelin, 56 ans, comparaissent notamment pour importation de stupéfiants en bande organisé"
+            "e - un crime passible de trente ans de réclusion - et corruption passive, devant la cour d'assises s"
+            "péciale, composée de magistrats professionnels, sans jury. </p>\n<p>Les deux hommes aux cheveux bruns"
+            " coupés courts, l'un la mâchoire carrée, l'autre le visage émacié, ont pénétré dans le box des accus"
+            "és, déclinant d'un ton grave leur identité et leur ancienne profession.</p>\n<p>Ils ont reconnu avoir"
+            ' permis sept à neuf passages de "mules" aux valises chargées de cocaïne en les aidant à contourner l'
+            "es contrôles de l'aéroport de Roissy, entre 2010 et le 25 janvier 2015, date de leur interpellation."
+            " Ils venaient de tenter de faire sortir deux valises contenant chacune 20 kilos de drogue. </p>\n<p>A"
+            " l'autre extrémité du box, figure Kamel Berkaoui, 43 ans, patron présumé du réseau démantelé à la su"
+            "ite d'une enquête dénommée \"Excédent bagage\", menée par l'Office central pour la répression du trafi"
+            "c illicite des stupéfiants (Ocrtis). </p>\n<p>Neuf membres présumés de son organisation sont égalemen"
+            "t jugés par la cour. </p>\n<p>Les enquêteurs n'ont pas réussi à déterminer quelle quantité de drogue "
+            "avait pu être importée depuis Punta Cana en République dominicaine. </p>\n<p>Les ex-policiers auraien"
+            't empoché au total entre 540.000 et 620.000 euros, à raison de 40.000 euros par "mule" récupérée, se'
+            "lon une estimation fournie en interrogatoire par Clément Geisse. </p>\n<p>Le verdict est attendu le 7"
+            " février. </p>\n<p>asl/tib/pid/cac</p>\n\n"
+        )
         self.assertEqual(item["body_html"], expected_body)
 
     def test_empty_headline(self):
-        news_lines = self.xml_root.find('NewsItem/NewsComponent/NewsLines')
-        headline = news_lines.find('HeadLine')
+        news_lines = self.xml_root.find("NewsItem/NewsComponent/NewsLines")
+        headline = news_lines.find("HeadLine")
         news_lines.remove(headline)
         # set urgency
-        urgency = self.xml_root.find('NewsItem/NewsManagement/Urgency')
-        urgency.attrib['FormalName'] = '1'
+        urgency = self.xml_root.find("NewsItem/NewsManagement/Urgency")
+        urgency.attrib["FormalName"] = "1"
 
         parser = BelgaAFPNewsMLOneFeedParser()
-        item = parser.parse(self.xml_root, {'name': 'test'})
-        assert item[0]['headline'] == (
+        item = parser.parse(self.xml_root, {"name": "test"})
+        assert item[0]["headline"] == (
             "URGENT: Le procès de deux anciens fonctionnaires de la police aux frontières (PAF) de l'aéroport parisien"
             " Roissy-Charles de Gaulle, accusés d'avoir facilité l'importation de cocaïne de retour de République "
             "dominicaine, s'est ouvert lundi devant un tribunal à Paris."
