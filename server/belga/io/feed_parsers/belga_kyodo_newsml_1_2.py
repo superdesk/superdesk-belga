ee9481a8ba4bf624b61f8fc7856bcc4248420d08
devketanpro <73937490+devketanpro@users.noreply.github.com>
2022-11-30 19:51:56 +0530

    Fix the mapping of the City field in the Kyodo parser [SDBELGA-693] (#413)
    * Fix the mapping of the City field in the Kyodo parser [SDBELGA-693]
    
    * Update testcases
    
    * bump eve-elastic vesrion
    
    * refactored the code
    
    * Undo changes
    
    * reformat code using black

--- server/belga/io/feed_parsers/belga_kyodo_newsml_1_2.py
+++ server/belga/io/feed_parsers/belga_kyodo_newsml_1_2.py
@@ -15,11 +15,25 @@ from .base_belga_newsml_1_2 import BaseBelgaNewsMLOneFeedParser
 class BelgaKyodoNewsMLOneFeedParser(BaseBelgaNewsMLOneFeedParser):
     """Feed Parser for Belga specific Kyodo NewsML."""
 
-    NAME = 'belga_kyodo_newsml12'
-    label = 'Belga specific Kyodo News ML 1.2 Parser'
+    NAME = "belga_kyodo_newsml12"
+    label = "Belga specific Kyodo News ML 1.2 Parser"
 
     def can_parse(self, xml):
-        return xml.tag == 'NewsML'
+        return xml.tag == "NewsML"
+
+    # SDBELGA - 693
+    def parse(self, xml, provider=None):
+        items = super().parse(xml, provider)
+        location_el = xml.find(
+            "NewsItem/NewsComponent/ContentItem/DataContent/nitf/body/body.head/dateline/location"
+        )
+        if location_el is not None:
+            for item in items:
+                item.setdefault("extra", {})["city"] = location_el.text
+
+        return items
 
 
-register_feed_parser(BelgaKyodoNewsMLOneFeedParser.NAME, BelgaKyodoNewsMLOneFeedParser())
+register_feed_parser(
+    BelgaKyodoNewsMLOneFeedParser.NAME, BelgaKyodoNewsMLOneFeedParser()
+)
