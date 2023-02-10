f93149f2eaba91b9bb7b6a5173320b161057c88a
devketanpro <devketanpro11@gmail.com>
2023-02-10 17:16:11 +0530

    update logic

--- server/belga/search_providers.py
+++ server/belga/search_providers.py
@@ -352,7 +352,8 @@ class Belga360ArchiveSearchProvider(superdesk.SearchProvider, BelgaNewsMLMixin):
                     flags=re.IGNORECASE,
                 )
                 if highlighted_value[1]:
-                    doc["es_highlight"] = {field: [highlighted_value[0]]}
+                    doc["es_highlight"] = doc.get("es_highlight", {})
+                    doc["es_highlight"][field] = [highlighted_value[0]]
 
     def find(self, query, params=None):
         api_params = {
