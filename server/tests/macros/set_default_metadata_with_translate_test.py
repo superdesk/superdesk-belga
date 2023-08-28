76bdf9a05b86f6d4f78b3de64ab599e2fb62b5f7
devketanpro <devketanpro11@gmail.com>
2023-08-28 18:54:31 +0530

    add testcases

--- server/tests/macros/set_default_metadata_with_translate_test.py
+++ server/tests/macros/set_default_metadata_with_translate_test.py
@@ -575,3 +575,182 @@ class SetDefaultMetadataWithTranslateTestCase(TestCase):
         )
 
         self.assertEqual(item["subject"], new_item["subject"])
+
+
+    def test_duplicate_item(self):
+        # SDBELGA-538
+
+        self.app.data.insert(
+            "filter_conditions",
+            [
+                {
+                    "_id":  ObjectId('6d385f17fe985ec5e1a78b49'),
+                    "operator": "in",
+                    "field": "services-products",
+                    "value": "BIN/ALG",
+                    "name": "Bin/alg filter_c",
+                },
+                {
+                    "_id":  ObjectId('7d385f17fe985ec5e1a78b49'),
+                    "operator": "in",
+                    "field": "services-products",
+                    "value": "BIN/ECO",
+                    "name": "Bin/eco filter_c",
+                },
+            ],
+        )
+        content_filter = [
+            {
+                "_id": 1,
+                "name": "Bin/ALG Filter",
+                "content_filter": [{"expression": {"fc": [ObjectId('6d385f17fe985ec5e1a78b49')]}}],
+            },
+            {
+                "_id": 2,
+                "name": "Bin/ECO Filter",
+                "content_filter": [{"expression": {"fc": [ObjectId('7d385f17fe985ec5e1a78b49')]}}],
+            },
+        ]
+        self.app.data.insert("content_filters", content_filter)
+
+        self.app.data.insert(
+            "desks",
+            [
+                {
+                    "_id": ObjectId('5d385f17fe985ec5e1a78b49'),
+                    "name": "Politic Desk",
+                    "default_content_profile": "belga_text-1",
+                    "default_content_template": "content_template_1",
+                    "desk_language": "fr",
+                    "source": "politic",
+                },
+                {
+                    "_id": ObjectId('5d385f17fe985ec5e1a78b40'),
+                    "name": "Sports Desk",
+                    "default_content_profile": "belga_text-2",
+                    "default_content_template": "content_template_2",
+                    "desk_language": "en",
+                    "source": "sports",
+                },
+            ],
+        )
+        self.app.data.insert(
+            "stages",
+            [
+                {
+                    "_id": ObjectId('5d385f17fe985ec5e1a88b49'),
+                    "name": "Incoming Stage",
+                    "default_incoming": True,
+                    "desk_order": 1,
+                    "content_expiry": None,
+                    "working_stage": False,
+                    "is_visible": True,
+                    "desk": ObjectId('5d385f17fe985ec5e1a78b49'),
+                },
+                {
+                    "_id":  ObjectId('5d385f17fe985ec5e1a89b49'),
+                    "name": "Incoming Stage",
+                    "default_incoming": True,
+                    "desk_order": 2,
+                    "content_expiry": None,
+                    "working_stage": False,
+                    "is_visible": True,
+                    "desk":  ObjectId('5d385f17fe985ec5e1a78b40'),
+                },
+            ],
+        )
+
+        self.app.data.insert(
+            "content_templates",
+            [
+                {
+                    "_id": "content_template_1",
+                    "template_name": "belga text one",
+                    "is_public": True,
+                    "data": {
+                        "profile": "belga_text",
+                        "type": "text",
+                        "pubstatus": "usable",
+                        "format": "HTML",
+                        "headline": "",
+                        "language": "en",
+                        "keywords": ["some", "keyword"],
+                        "body_html": "",
+                    },
+                    "template_type": "create",
+                },
+                {
+                    "_id": "content_template_2",
+                    "template_name": "belga text two",
+                    "is_public": True,
+                    "data": {
+                        "profile": "belga_text",
+                        "type": "text",
+                        "pubstatus": "usable",
+                        "format": "HTML",
+                        "headline": "",
+                        "language": "en",
+                        "keywords": ["some", "keyword"],
+                        "body_html": "",
+                    },
+                    "template_type": "create",
+                },
+            ],
+        )
+
+        destination = [
+            {
+                "_id": "desti_1",
+                "is_active": True,
+                "name": "BIN/ALG",
+                "filter": "1",
+                "desk": "123",
+                "stage": "213",
+                "macro": "Set Default Metadata With Translate",
+            },
+            {
+                "_id": "desti_2",
+                "is_active": True,
+                "name": "BIN/ECO",
+                "filter": "2",
+                "desk": "456",
+                "stage": "214",
+                "macro": "Set Default Metadata With Translate",
+            },
+        ]
+        self.app.data.insert("internal_destinations", destination)
+
+        item = {
+            "_id": "urn:newsml:localhost:5000:2019-12-10T14:43:46.224107:d13ac5ae-7f43-4b7f-89a5-2c6835389564",
+            "guid": "urn:newsml:localhost:5000:2019-12-10T14:43:46.224107:d13ac5ae-7f43-4b7f-89a5-2c6835389564",
+            "headline": "test headline",
+            "slugine": "test slugline",
+            "state": "published",
+            "type": "text",
+            "subject": [
+                {
+                    "name": "BIN/ALG",
+                    "qcode": "BIN/ALG",
+                    "parent": "BIN",
+                    "scheme": "services-products",
+                },
+                {
+                    "name": "BIN/ECO",
+                    "qcode": "BIN/ECO",
+                    "parent": "BIN",
+                    "scheme": "services-products",
+                },
+            ],
+            "keywords": ["foo", "bar"],
+            "language": "fr",
+        }
+
+        self.app.data.insert("archive", [item])
+        self.assertRaises(
+            StopDuplication,
+            set_default_metadata_with_translate,
+            item,
+            dest_desk_id=ObjectId('5d385f17fe985ec5e1a78b49'),
+            dest_stage_id=ObjectId('5d385f17fe985ec5e1a88b49'),
+            internal_destination = destination[0]
+        )
