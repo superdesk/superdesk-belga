922b780da9b1a4aa0b41659f0202d9b8d7a27ff7
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-05-02 12:10:04 +0300

    Revert server config

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -101,11 +101,6 @@ module.exports = function(grunt) {
             'scheduled': 1
         },
 
-        server: {
-            "url": "http://127.0.0.1:5000/api",
-            "ws": "ws://127.0.0.1:5100/ws"
-        },
-
         list: {
             priority: [
                 'urgency'
