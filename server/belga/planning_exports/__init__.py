cebd089523af7678f926c03109564dc1913380f7
devketanpro <devketanpro11@gmail.com>
2024-06-18 10:08:50 +0530

    fix:Sports Advisory custom layout issues[SDBELGA-822]

--- server/belga/planning_exports/__init__.py
+++ server/belga/planning_exports/__init__.py
@@ -1,15 +1,7 @@
-from .france_news_event_list import format_event_french
-from .dutch_news_events_list import format_event_dutch
+from .format_news_events_week import format_event_for_week
 from .format_news_events_tommorow import format_event_for_tommorow
 
 
 def init_app(app):
-    app.jinja_env.globals.update(
-        format_event_french=format_event_french
-    )
-    app.jinja_env.globals.update(
-        format_event_dutch=format_event_dutch
-    )
-    app.jinja_env.globals.update(
-        format_event_for_tommorow=format_event_for_tommorow
-    )
+    app.jinja_env.globals.update(format_event_for_week=format_event_for_week)
+    app.jinja_env.globals.update(format_event_for_tommorow=format_event_for_tommorow)
