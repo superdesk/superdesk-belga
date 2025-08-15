1b6338fd40f943fb1ef2009790b95b651cc695cf
Ivan Jelić <99324896+IvanJelicSF@users.noreply.github.com>
2025-08-15 17:28:54 +0200

    Enable embargo

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -65,11 +65,6 @@ module.exports = function(grunt) {
             assignments: true
         },
 
-        ui: {
-            publishEmbargo: false,
-            sendEmbargo: false,
-        },
-
         langOverride: {
             'en': {
                 'slugline': 'topic',
