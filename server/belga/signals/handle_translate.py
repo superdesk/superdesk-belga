cc6ea4074539ccdc0fc6be13a0b4dcc674e6fc55
Petr Jasek <jasekpetr@gmail.com>
2026-09-21 15:27:31 +0200

    keep video associations in translations
    SDBELGA-1163

--- server/belga/signals/handle_translate.py
+++ server/belga/signals/handle_translate.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from apps.archive.common import validate_schedule
 from superdesk.errors import SuperdeskApiError
 
@@ -11,23 +10,8 @@ from superdesk.metadata.item import (
 )
 
 
-def is_belga_archive(item):
-    return item.get("type") == CONTENT_TYPE.TEXT and item.get("extra", {}).get(
-        "bcoverage"
-    )
-
-
-def is_video(item):
-    return item.get("type") == CONTENT_TYPE.VIDEO
-
-
 def handle_duplicate(sender, item, original, operation):
     if operation == "translate":
-        # remove all belga-archive-360/video associations
-        for k, v in deepcopy(item.get("associations", {})).items():
-            if v is not None and (is_belga_archive(v) or is_video(v)):
-                del item.get("associations", {})[k]
-
         # keep schedule settings if still valid
         if original.get(ITEM_STATE) == CONTENT_STATE.SCHEDULED and original.get(
             PUBLISH_SCHEDULE
